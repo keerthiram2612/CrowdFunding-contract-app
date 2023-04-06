0ccb9671f06a52c53f3b1ee5fbf98fc8ac13c7c2
keerthiram2612 <keerthiram2612@gmail.com>
2023-04-06 05:57:12 +0530

    downloading the react app

---
+++ jouleapp/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App(){
+  return(
+    <div className="bg-black">
+    <h1 className=" text-red">Hello world</h1>
+    </div>
+  )
+}
+export default App;
+
+
+
